94bf44b57e921ff8bdb499a05b72b6ed35d633f7
zadrot1na <you@example.com>
2024-12-02 15:09:39 +0200

    VCS created

---
+++ database/factories/BlogPostFactory.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\BlogPost;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends //Factory<BlogPost>
+ */
+class BlogPostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $title = $this->faker->sentence(rand(3, 8), true);
+        $txt = $this->faker->realText(rand(1000, 4000));
+        $isPublished = rand(1, 5) > 1;
+
+        $createdAt = $this->faker->dateTimeBetween('-3 months', '-2 months');
+
+        $data = [
+            'category_id' => rand(1, 11),
+            'user_id' => (rand(1, 5) == 5) ? 1 : 2,
+            'title' => $title,
+            'slug' => Str::slug($title),
+            "excerpt" => $this->faker->text(rand(40, 100)),
+            'content_raw' => $txt,
+            'content_html' => $txt,
+            'is_published' => $isPublished,
+            'published_at' => $isPublished ? $this->faker->dateTimeBetween('-2 months',
+                '-1 days') : null,
+            'created_at' => $createdAt,
+            'updated_at' => $createdAt,
+
+        ];
+
+        return $data;
+    }
+}
