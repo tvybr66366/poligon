1eb204791b95dfad875b73827eb5321a942c2e06
zadrot1na <you@example.com>
2024-12-13 00:01:19 +0200

    Laravel урок №:16 [ Страница редактирования категории ]

--- app/Http/Controllers/Blog/Admin/CategoryController.php
+++ app/Http/Controllers/Blog/Admin/CategoryController.php
@@ -4,7 +4,11 @@ namespace App\Http\Controllers\Blog\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\BlogCategory;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Response;
 
 class CategoryController extends Controller
 {
@@ -13,7 +17,7 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        $paginator = BlogCategory::query()->paginate(5);
+        $paginator = BlogCategory::query()->paginate(15);
 
         return view('blog.admin.category.index', compact('paginator'));
     }
@@ -38,19 +42,29 @@ class CategoryController extends Controller
 
     /**
      * Show the form for editing the specified resource.
+     *
+     * @param int $id
+     * @return Factory|View|Application|\Illuminate\View\View
      */
-    public function edit(string $id)
+    public function edit(int $id)
     {
-        dd(__METHOD__);
+        $item = BlogCategory::query()->findOrFail($id);
+        $categoryList = BlogCategory::all();
 
+        return view('blog.admin.category.edit',
+            compact('item', 'categoryList'));
     }
 
     /**
      * Update the specified resource in storage.
+     *
+     * @param Request $request
+     * @param int $id
+     *
+     * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, int $id)
     {
-        dd(__METHOD__);
-
+        dd(__METHOD__, $request->all(), $id);
     }
 }
